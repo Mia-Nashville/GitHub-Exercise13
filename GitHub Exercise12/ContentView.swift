fee61dbc48b39180d90723782f1459d9eaa810da
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2023-12-27 17:59:24 -0600

    Initial Commit

---
+++ GitHub Exercise12/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  GitHub Exercise12
+//
+//  Created by Satoshi Mitsumori on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Mia!").fontWeight(.semibold) + Text(" We have some Work to do V1.0" )
+                .foregroundStyle(Color.blue)
+            Text("Version 1.2")
+                .foregroundStyle(Color.secondary)
+                .fontWeight(.medium)
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Exercise12/GitHub_Exercise12App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Exercise12App.swift
+//  GitHub Exercise12
+//
+//  Created by Satoshi Mitsumori on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Exercise12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
